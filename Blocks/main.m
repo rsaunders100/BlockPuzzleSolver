2f9c3ff4a147e6bb9d25766ac9976e9da614e960
Robert Saunders <rsaunders100@gmail.com>
2011-12-27 16:04:12 +0000

    Initial Commit

---
+++ Blocks/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Blocks
+//
+//  Created by Robert Saunders on 27/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
